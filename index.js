3b12c54456ce691577b61da4388b0371ea4e9302
Lucy <lucy.d.bullen@gmail.com>
2022-05-22 19:36:06 -0600

    commit tree fe6801cb69fc14b2fe752c0b7c1b16dde60c6905

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+  const newUser = {
+    name: userName,
+    email: userEmail
+  } 
+
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(newUser)
+  })
+  .then(resp => resp.json())
+  .then(json => postIdToDOM(json))
+  .catch(error => errorMessage(error))
+}
+
+function postIdToDOM(json) {
+  const id = json.id
+  const ul = document.createElement("ul");
+  const li = document.createElement("li");
+  li.textContent = id;
+  ul.appendChild(li);
+  document.querySelector("body").appendChild(ul);
+}
+
+function errorMessage(error) {
+  const message = error.message;
+  const p = document.createElement("p");
+  p.textContent = message;
+  document.querySelector("body").appendChild(p);
+}
+
+submitData('user1', 'Lucy');
+
